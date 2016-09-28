dfb83e066114fa92c8911091952d4e25f3b96341
Maria Legaspi <legaspange@shish-mahal.ecs.vuw.ac.nz>
2016-09-28 17:46:41 +1300

    Setting up networking - writing methods to add clients to the server and send strings to all clients.

--- src/kaukau/control/Client.java
+++ src/kaukau/control/Client.java
@@ -1,5 +1,30 @@
 package kaukau.control;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Date;
 
+/**
+ * Based off http://stackoverflow.com/questions/29545597/multiplayer-game-in-java-connect-client-player-to-game-that-was-created-by-ot
+ * @author Maria Legaspi
+ *
+ */
+
+public class Client{
+    public static void main(String[] args) throws Exception  {
+        Socket socket = new Socket("127.0.0.1", Server.portNumber);
+        BufferedWriter writerChannel = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        BufferedReader readerChannel = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String line;
+
+        writerChannel.write("New client\n");
+        writerChannel.flush();
+
+        while ((line = readerChannel.readLine()) != null)  {
+            System.out.println(line);
+        }
+    }
 }
---
+++ src/kaukau/control/ClientThread.java
@@ -0,0 +1,19 @@
+package kaukau.control;
+
+import java.net.Socket;
+
+/**
+ * Thread for concurrency.
+ * @author Maria Legaspi
+ *
+ */
+
+public class ClientThread extends Thread{
+	private Server server;
+	private Socket clientSock;
+
+	public ClientThread(Server server, Socket clientSock){
+		this.server = server;
+		this.clientSock = clientSock;
+	}
+}
