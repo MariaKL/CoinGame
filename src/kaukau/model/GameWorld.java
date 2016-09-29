d260a61d8ca273e967c6adc196304b6b880fd60b
Vivienne Yapp <yappvivi@afrika.ecs.vuw.ac.nz>
2016-09-29 14:21:26 +1300

    GameWorld and Room classes to create a new gameand board

--- src/kaukau/model/GameWorld.java
+++ src/kaukau/model/GameWorld.java
@@ -3,86 +3,137 @@ package kaukau.model;
 import java.util.HashMap;
 
 import kaukau.model.Room.TileType;
+import kaukau.storage.ReadXMLFile;
 
 import java.awt.Point;
 import java.io.IOException;
+import org.w3c.dom.Document;
+
 
 public class GameWorld {
-	
+
 	private Room[][] map;
-	private HashMap<String, Player> players;
+	private HashMap<String, Player> players = new HashMap<String, Player>();
 	private final int MAX_PLAYERS = 4;
-	
+
 	public GameWorld(String filename) throws IOException{
 		map = new Room[1][1];
 		map[0][0] = new Room("classroom", filename);
-		this.players = new HashMap<String, Player>();
 	}
-	
+
+	/**
+	 * Add new player to the current game. If the name of player is already in the list, it returns False.
+	 * @param name
+	 * @return True if the player was successfully added, otherwise False.
+	 */
 	public synchronized boolean addPlayer(String name){
-		if (players.size() >= MAX_PLAYERS) return false;
+		if (players.size() >= MAX_PLAYERS || players.containsKey(name)) return false;
 		Room room = map[0][0];
-		this.players.put(name, new Player(name, room.getTileAt(new Point(1, 1)), Direction.EAST));
+		this.players.put(name, new Player(name, room.getTileAt(new Point(5, 0)), Direction.EAST));
 		return true;
 	}
-	
+
+	/**
+	 * Move player to the new position if the tile of the new position is not occupy. Otherwise, it returns False.
+	 * @param name Player's name
+	 * @param p The new position
+	 * @return True if the player is move to the new position, otherwise False.
+	 */
 	public synchronized boolean movePlayer(String name, Point p){
 		Room room = map[0][0];
 		if (room.width() <= p.x || room.height() <= p.y) return false;
 		Player player = players.get(name);
 		Tile tile = room.getTileAt(p);
-		if (tile.getTileType() == TileType.EMPTY && room.isTileOccupied(p)){
-			player.setLocation(tile);
-			return true;
+
+		// if the tile is emptyTile and not occupy, then move player to this new position
+		if (tile.getTileType() == TileType.EMPTY){
+			EmptyTile e = (EmptyTile) tile;
+			EmptyTile oldPos = (EmptyTile) player.getLocation();
+			if (!e.isTileOccupied()){
+				player.setLocation(tile);
+				e.addPlayer(player);
+				oldPos.removePlayer();
+				return true;
+			}
 		}
 		return false;
 	}
-	
-	public synchronized boolean pickupAnItem(Player player, Point p){
+
+	public synchronized boolean pickupAnItem(String name, Point p){
 		Room room = map[0][0];
 		if (room.width() <= p.x || room.height() <= p.y) return false;
+		Player player = players.get(name);
 		Tile tile = room.getTileAt(p);
-		if (room.containsItem(p)){
-			return true;
+		if (tile instanceof EmptyTile){
+			EmptyTile e = (EmptyTile) tile;
+			if (e.containsItem()){
+				PickupableItem item = e.getItem();
+				player.addToBag(item);
+				item.setOwner(player);
+				e.removeItem();
+				return true;
+			}
 		}
 		return false;
 	}
-	
+
 	public synchronized boolean performAction(String name, String action, Point p) {
-		/*if (action.equals("Pickup")) {
-			return pickupThisItem(player);
-		} else if(action.equals("Drop")){
-			return dropThisItem(player);
-		} else {
-			throw new IllegalArgumentException("Unknown action: " + action);
-		}*/
 		return false;
 	}
-	
+
 	private Room[][] getMap(){
 		return map;
 	}
-	
+
 	private Player getPlayer(String name){
 		if (players.containsKey(name)){
 			return players.get(name);
 		}
 		return null;
 	}
-	
+
+	/**
+	 * Return the current board of the game.
+	 * @return
+	 */
+	public Room[][] getGameMap(){
+		return map;
+	}
+
+
 	// testing
 	public static void main (String[] args) throws IOException{
-		GameWorld game = new GameWorld("tiles.txt");
+		GameWorld game = new GameWorld("room.xml");
 		game.addPlayer("Vivienne");
 		Room[][] rooms = game.getMap();
 		Room room = rooms[0][0];
+
 		System.out.println(room.getTileAt(new Point(1,1)).getTileType());
 		System.out.println(game.getPlayer("Vivienne"));
 		Player player = game.getPlayer("Vivienne");
-		System.out.println(game.movePlayer(player.getName(), new Point(2, 1)));
-		System.out.println(room.getTileAt(new Point(2,1)).getTileType());
-		System.out.println(game.getPlayer("Vivienne"));
-	}
+		System.out.println(player.facingDirection());
+		//System.out.println(game.movePlayer(player.getName(), new Point(2, 1)));
+		//System.out.println(room.getTileAt(new Point(2,1)).getTileType());
+		System.out.println("move " + game.movePlayer(player.getName(), new Point(6, 0)));
+		System.out.println(game.pickupAnItem(player.getName(), new Point(6, 0)));
+		System.out.println(player);
+		System.out.println(player.facingDirection());
 
+		for (Item item : player.getBag().getStorage()){
+			System.out.println(item.getName());
+		}
+
+		Tile[][] tiles = room.getRoomBoard();
+		EmptyTile item = (EmptyTile) tiles[6][0];
+		System.out.println(item.containsItem());
+		System.out.println(item.isTileOccupied());
+
+		for (int x = 0; x < 7; x++){
+			for (int y = 0; y < 7; y++){
+				System.out.print(room.getTileAt(new Point(x,y)).getTileType() + ", ");
+			}
+			System.out.println();
+		}
+	}
 
 }
--- src/kaukau/model/Room.java
+++ src/kaukau/model/Room.java
@@ -5,49 +5,108 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import kaukau.storage.ReadXMLFile;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
 import java.awt.Point;
 
 public class Room {
-	
-	private Tile[][] board;
-	private HashMap<Point, PickupableItem> items = new HashMap<Point, PickupableItem>();
+
+	private static final int ROOM_WIDTH = 7;
+	private static final int ROOM_HEIGHT = 7;
+	private Tile[][] board = new Tile[ROOM_WIDTH][ROOM_HEIGHT];
 	private String name;
-	private int width, height;
 	private int keyCode = 0;
-	
+
 	public enum TileType{
 		WALL, EMPTY, DOOR;
 	}
-	
-	public Room(String name, String filename) throws IOException{
+
+	public Room(String name, String doc) throws IOException{
 		this.name = name;
-		createRoomFromFile(filename);
+		createRoomFromFile(doc);
 	}
 
-	private void createRoomFromFile(String filename) throws IOException {
-		FileReader fr = new FileReader(filename);		
+	public Room(String doc) throws IOException{
+		createRoomFromFile(doc);
+	}
+
+	public void createRoomFromFile(String filename){
+		try {
+			Document doc = new ReadXMLFile().createDocument(filename);
+			NodeList nList = doc.getElementsByTagName("room");
+
+    		for (int temp = 0; temp < nList.getLength(); temp++) {
+    			Node nNode = nList.item(temp);
+    			//System.out.println("\nCurrent Element :" + nNode.getNodeName());
+
+    			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+    				Element eElement = (Element) nNode;
+    				this.name = eElement.getAttribute("name");
+
+    				for (int x = 0; x < ROOM_HEIGHT; x++){
+    					// get the value line by line from XML file e.g. a line is <L0></L0>
+	    				String line = eElement.getElementsByTagName("L"+String.valueOf(x)).item(0).getTextContent();
+
+	    				for(int y = 0;y!= ROOM_WIDTH;++y) {
+	    					char c = line.charAt(y);
+	    					switch (c) {
+		    					case 'W' :
+		    						addWall(x, y);
+		    						break;
+		    					case 'D':
+		    						addDoor(x, y);
+		    						break;
+		    					case 'C':
+		    						addEmptyTile(x, y);
+		    						break;
+		    					case 'N':
+		    						addItem(x,y);
+		    						break;
+	    					}
+	    				}
+
+	    			}
+    			}
+
+    		}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/*private void createRoomFromFile(String filename) throws IOException {
+		FileReader fr = new FileReader(filename);
 		BufferedReader br = new BufferedReader(fr);
 		ArrayList<String> lines = new ArrayList<String>();
 		int width = -1;
-		String line;		
+		String line;
 		while((line = br.readLine()) != null) {
 			lines.add(line);
-			
+
 			// now sanity check
-			
-			if(width == -1) {				
+
+			if(width == -1) {
 				width = line.length();
-			} else if(width != line.length()) {				
+			} else if(width != line.length()) {
 				throw new IllegalArgumentException("Input file \"" + filename + "\" is malformed; line " + lines.size() + " incorrect width.");
-			}			
+			}
 		}
-		this.width = width;
-		this.height = lines.size();
+		//this.width = width;
+		//this.height = lines.size();
 		board = new Tile[width][lines.size()];
 		for(int y=0;y!=lines.size();++y) {
 			line = lines.get(y);
 			for(int x=0;x!=width;++x) {
 				char c = line.charAt(x);
+				System.out.println(c);
 				switch (c) {
 					case 'W' :
 						addWall(x, y);
@@ -60,53 +119,56 @@ public class Room {
 						break;
 					case 'N':
 						addItem(x,y);
-						break;					
+						break;
 				}
 		}
-		
+
 	     }
 
 	}
-
+*/
 	private void addDoor(int x, int y) {
 		board[x][y] = new Tile(TileType.DOOR, x, y);
-		
 	}
 
 	private void addWall(int x, int y) {
 		board[x][y] = new Tile(TileType.WALL, x, y);
 	}
-	
+
 	private void addEmptyTile(int x, int y) {
-		board[x][y] = new Tile(TileType.EMPTY, x, y);
+		EmptyTile tile = new EmptyTile(TileType.EMPTY, x, y);
+		board[x][y] = tile;
 	}
-	
+
 	private void addItem(int x, int y) {
-		board[x][y] = new Tile(TileType.EMPTY, x, y);
+		EmptyTile tile = new EmptyTile(TileType.EMPTY, x, y);
 		Key key = new Key(++keyCode, "Key 1");
+		tile.addItem(key);
+		board[x][y] = tile;
 		Point p = new Point(x, y);
-		items.put(p, key);
 	}
-	
+
+
 	public Tile getTileAt(Point p){
+		if (p.x >= ROOM_WIDTH || p.y >= ROOM_HEIGHT) return null;
 		return board[p.x][p.y];
 	}
-	
+
+	// if the tile is not EmptyTile, then it is occupy.
 	public boolean isTileOccupied(Point p){
-		if (!items.containsKey(p) && board[p.x][p.y].getTileType() == TileType.EMPTY) return false;
+		Tile tile = board[p.x][p.y];
+		if (tile.getTileType() == TileType.EMPTY){
+			EmptyTile e = (EmptyTile) tile;
+			return e.isTileOccupied();
+		}
 		return true;
 	}
-	
-	/**
-	 * Determines if the point of this room contains an item or not.
-	 * @param p 
-	 * @return TRUE if there is one, otherwise FALSE
-	 */
-	public boolean containsItem(Point p){
-		if (items.containsKey(p)) return true;
-		return false;
+
+	public Tile[][] getRoomBoard(){
+		return board;
 	}
-	
-	public int width(){ return width; }
-	public int height(){ return height; }
+
+	public int width(){ return ROOM_WIDTH; }
+	public int height(){ return ROOM_HEIGHT; }
+	public String getName(){ return name; }
 }
