f8276a4ad8264f74ed9189aef62b6e5fbd786778
Maria Legaspi <legaspange@fyber.ecs.vuw.ac.nz>
2016-10-08 16:47:17 +1300

    Client movements now send to server to update main game copy.

--- src/kaukau/control/Client.java
+++ src/kaukau/control/Client.java
@@ -29,26 +29,41 @@ public class Client extends Thread {
 	private int uid;
 	private boolean initialRun = true;
 
+	private ObjectInputStream input;
+	private ObjectOutputStream output;
+
 
 	/**
 	 * Creates a client for the player.
 	 */
 	public Client(ApplicationWindow aw){
 		// makes a new socket and sets input/output streams
-		try{
 			// sets references to the game and ui window
 			this.aw = aw;
 			this.game = aw.game;
 			System.out.println("Set window and game");
 			// sets networking fields
+	        setupSocket();
+	}
+
+	/**
+	 * Connects the socket to the server.
+	 */
+	private void setupSocket(){
+		try{
+			// sets networking fields
 	        sock = new Socket(address, Server.portNumber);
+	        output = new ObjectOutputStream(sock.getOutputStream());
+	        input = new ObjectInputStream(sock.getInputStream());
 	        connected = true;
 	        System.out.println("Made socket");
 	    } catch(EOFException e){
 			System.out.println("Server not running.");
+			System.out.println("Single player game");
 		} catch(ConnectException e){
 			connected = false;
 			System.out.println("Server unavailable.");
+			System.out.println("Single player game");
 		} catch(IOException e){
 			e.printStackTrace();
 		}
@@ -59,14 +74,13 @@ public class Client extends Thread {
 		try {
 			// single player game
 			if(!connected){
-				System.out.println("Single player game begun");
-				closeClientSock();
+				setupSocket();
 				return;
 			}
 			// multiplayer game
 			if(initialRun){
 				// TODO: get client to read its uid from server BEFORE running client
-		        ObjectInputStream input = new ObjectInputStream(sock.getInputStream());
+//		        ObjectInputStream input = new ObjectInputStream(sock.getInputStream());
 				// checks if client is connected to the server
 				boolean accepted = false;
 				if(input!=null)
@@ -79,23 +93,21 @@ public class Client extends Thread {
 				}
 				uid = input.readInt();
 				System.out.println("New client uid: " + uid);
-		        ObjectInputStream readGame = new ObjectInputStream(sock.getInputStream());
-				game.fromByteArray((byte[])readGame.readObject());
 				initialRun = false;
 			}
 			// play game
 			System.out.println("Waiting for game updates");
 			boolean closed = false;
 			while(!closed){
-		        ObjectInputStream input = new ObjectInputStream(sock.getInputStream());
+//				ObjectInputStream readGame = new ObjectInputStream(sock.getInputStream());
 				System.out.println("Players size before update: " + game.getAllPlayers().size());
 				// wait for game updates from server
-//				game.fromByteArray((byte[])input.readObject());
+				game.fromByteArray((byte[])input.readObject());
 				System.out.println("Received game update");
 				System.out.println("Players size after update: " + game.getAllPlayers().size());
 			}
-			sock.close();
 		} catch(EOFException e){
+			e.printStackTrace();
 			System.out.println("Input not read");
 		} catch(ConnectException e){
 			System.out.println("Server not running");
@@ -104,6 +116,15 @@ public class Client extends Thread {
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
+//			finally{
+//			try {
+//				if(sock!=null)
+//					sock.close();
+//			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
 	}
 
 	/**
@@ -111,7 +132,7 @@ public class Client extends Thread {
 	 */
 	public void closeClientSock(){
 		try{
-			if(!sock.isClosed()){
+			if(sock!=null && !sock.isClosed()){
 				sock.close();
 			}
 		}
@@ -128,9 +149,11 @@ public class Client extends Thread {
 	public void sendAction(int code){
 		try {
 			if(connected){
-		        ObjectOutputStream output = new ObjectOutputStream(sock.getOutputStream());
+//		        ObjectOutputStream output = new ObjectOutputStream(sock.getOutputStream());
 				output.writeInt(uid);
 				output.writeInt(code);
+				output.flush();
+				System.out.println("Wrote command to server");
 			}
 		} catch (IOException e) {
 			// problem with sending to the server
--- src/kaukau/control/Server.java
+++ src/kaukau/control/Server.java
@@ -28,14 +28,18 @@ public class Server{
 	public static int portNumber = 4000;
 	// server sockets
 	private ServerSocket listener; // accepts clients
+	private Thread listeningThread;
+	private Thread commandThread;
 	// client sockets
 	private static HashMap<Integer, Socket> sockets = new HashMap<Integer, Socket>();
+	private static HashMap<Integer, ObjectInputStream> in = new HashMap<Integer, ObjectInputStream>();
+	private static HashMap<Integer, ObjectOutputStream> out = new HashMap<Integer, ObjectOutputStream>();
 	// commands received from clients
 	private Queue<String> commands = new LinkedList<String>();
 	// game
 	private static GameWorld game;
 	// current player to listen to
-	private static int player = 0;
+	private static int player = 1;
 
 	/**
 	 * Creates the listening socket and the two threads to connect clients and handle player actions.
@@ -47,10 +51,9 @@ public class Server{
 			listener = new ServerSocket(portNumber);
 			System.out.println("Created server");
 			// start connecting and listening to clients
-			Thread listeningThread = makeListeningThread();
-			Thread commandThread = makeCommandThread();
+			listeningThread = makeListeningThread();
+			commandThread = makeCommandThread();
 			listeningThread.start();
-			commandThread.start();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -64,20 +67,27 @@ public class Server{
 		Thread commandThread = new Thread() {
 		    public void run() {
 		    	try{
+		    		System.out.println("Waiting for commands");
 			    	while(atleastOneConnection()) {
+			    		System.out.println("At least one connection");
 	//					game.setState(Board.READY);
-						Thread.sleep(3000);
+//						Thread.sleep(3000);
 	//					game.setState(Board.PLAYING);
 
 						//TODO: go through each client and read input
 
 						while(!game.isOver()) {
+				    		System.out.println("Game continues");
 							Socket sock = sockets.get(player);
 							if(sock == null) System.out.println("No sock");
 							// read input from player
-							ObjectInputStream input = new ObjectInputStream(sock.getInputStream());
-							int uid = input.readInt(); // get player id
-							int dir = input.readInt(); // get movement
+//							ObjectInputStream input = new ObjectInputStream(sock.getInputStream());
+							ObjectInputStream input = getInputStream(sock);
+							if(input==null)
+								System.out.println("No inputstream for the sock");
+							// reads in commands
+							int uid = input.readInt();
+							int dir = input.readInt();
 							System.out.println("Read command from player " + uid + ", direction = " + dir);
 							// update game with player movement
 							switch(dir) {
@@ -89,14 +99,17 @@ public class Server{
 								case KeyEvent.VK_LEFT:
 								case KeyEvent.VK_KP_LEFT:
 									game.movePlayer(uid, Direction.WEST);
+									System.out.println(uid + ": Moved left");
 									break;
 								case KeyEvent.VK_UP:
 								case KeyEvent.VK_KP_UP:
 									game.movePlayer(uid, Direction.NORTH);
+									System.out.println(uid + ": Moved up");
 									break;
 								case KeyEvent.VK_DOWN:
 								case KeyEvent.VK_KP_DOWN:
 									game.movePlayer(uid, Direction.SOUTH);
+									System.out.println(uid + ": Moved down");
 									break;
 							}
 							System.out.println("Updated game");
@@ -104,19 +117,22 @@ public class Server{
 							updateAll();
 							//OR
 	//						sendToAll(uid + " " + dir);
-							Thread.sleep(3000);
+//							Thread.sleep(3000);
 						}
+			    		System.out.println("Game over");
 						// If we get here, then we're in game over mode
-						Thread.sleep(3000);
+//						Thread.sleep(3000);
 	//					// Reset board state
 	//					game.setState(Board.WAITING);
 	//					game.fromByteArray(state);
 					}
+		    		System.out.println("No connections");
 		    	} catch(IOException e){
 		    		e.printStackTrace();
-		    	} catch(InterruptedException e){
-		    		e.printStackTrace();
 		    	}
+//		    	catch(InterruptedException e){
+//		    		e.printStackTrace();
+//		    	}
 		    }
 		};
 		return commandThread;
@@ -129,16 +145,17 @@ public class Server{
 	public Thread makeListeningThread(){
 		Thread listeningThread = new Thread() {
 		    public void run() {
-		    	//TODO: allow for multiple players
 		        try{
 		        	System.out.println("Listening");
 		            while(game.getAllPlayers().size() < 2){ // max two players
 						Socket socket = listener.accept();
 						// if there are more than two players then don't accept the new player
+	            		ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+	            		ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
 		            	if(sockets.size() > 2){
-		            		ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+//		            		ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
 		    				output.writeBoolean(false);
-		    				output.writeUTF("Cannot accept another player");
+//		    				output.writeUTF("Cannot accept another player");
 				        	output.flush();
 		            	}
 		            	// otherwise accept a new player
@@ -146,16 +163,22 @@ public class Server{
 							// accept a new client
 							int uid = game.addPlayer();
 							sockets.put(uid, socket);
+							out.put(uid, output);
+							in.put(uid, input);
 							System.out.println("New socket: " + socket.getPort() + ", UID: " + uid);
 
-		            		ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+//		            		ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
 		    				output.writeBoolean(true);
+//				        	output.flush();
 							// send the client's uid to the client
 							output.writeInt(uid);
-				        	output.flush();
+//				        	output.flush();
 				        	// send new game to all clients
 							System.out.println("Server: number of players: " + game.getAllPlayers().size());
 							System.out.println("ALL CLIENTS ACCEPTED --- GAME BEGINS");
+							// starts listening for commands when a player is connected
+							if(!commandThread.isAlive())
+								commandThread.start();
 				        	// send new game to all players
 				        	updateAll();
 		            	}
@@ -201,9 +224,14 @@ public class Server{
         	  if(sock.isClosed())
         		  continue;
         	  // sends game to byte array to each client
-        	  ObjectOutputStream out = new ObjectOutputStream(sock.getOutputStream());
-        	  out.writeObject(game.toByteArray());
-        	  out.flush();
+//        	  ObjectOutputStream output = new ObjectOutputStream(sock.getOutputStream());
+        	  ObjectOutputStream output = getOutputStream(sock);
+        	  if(out==null){
+        		  System.out.println("No output stream initialised for this sock " + getUID(sock));
+        	  }
+        	  output.writeObject(game.toByteArray());
+        	  output.flush();
+        	  System.out.println("Sent updated game to player " + getUID(sock));
           }
     	  System.out.println("Sent updated game to players");
     	}
@@ -221,7 +249,8 @@ public class Server{
     	try{
           for(Socket sock: sockets.values()){
         	  // sends game to byte array to each client
-        	  ObjectOutputStream output = new ObjectOutputStream(sock.getOutputStream());
+//        	  ObjectOutputStream output = new ObjectOutputStream(sock.getOutputStream());
+        	  ObjectOutputStream output = getOutputStream(sock);
         	  output.writeUTF(message);
         	  output.flush();
           }
@@ -232,22 +261,6 @@ public class Server{
     }
 
     /**
-<<<<<<< HEAD
-     * Returns the id of a player given the socket.
-     * @param sock
-     * @return
-     */
-    private static Integer getUid(Socket sock){
-    	for(Integer i: sockets.keySet()){
-    		if(sockets.get(i) == sock)
-    			return i;
-    	}
-    	return -1;
-    }
-
-	/**
-=======
->>>>>>> c5df32e9ef6a6180c3f3c83a5f2b2a2722265af8
      * Closes all sockets.
      */
     public void closeAll(){
@@ -258,6 +271,42 @@ public class Server{
 				System.out.println("Unable to close socket");
 			}
     	}
+    }
+
+    /**
+     * Returns the input stream of a given sock.
+     * @param sock
+     * @return
+     */
+    private static ObjectInputStream getInputStream(Socket sock){
+    	for(Integer i: sockets.keySet())
+    		if(sockets.get(i).equals(sock))
+    			return in.get(i);
+    	return null;
+    }
+
+    /**
+     * Returns the output stream of a given sock.
+     * @param sock
+     * @return
+     */
+    private static ObjectOutputStream getOutputStream(Socket sock){
+    	for(Integer i: sockets.keySet())
+    		if(sockets.get(i).equals(sock))
+    			return out.get(i);
+    	return null;
+    }
+
+    /**
+     * Returns the uid associated with this socket.
+     * @param sock
+     * @return
+     */
+    private static int getUID(Socket sock){
+    	for(Integer i: sockets.keySet())
+    		if(sockets.get(i).equals(sock))
+    			return i;
+    	return -1;
     }
 
     /**
